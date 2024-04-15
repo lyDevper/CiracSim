1f238e02e631e279d78b6a977d3d8087f1e3f891
lyDevper <luckydekdee@gmail.com>
2024-04-15 15:14:30 +0700

    added Goal

--- scripts/index.js
+++ scripts/index.js
@@ -11,12 +11,16 @@ let robot = new Robot(canvas1, coor1);
 let circleLine = new CircleLine(canvas1, coor1);
 let ball = new Ball(canvas1, coor1);
 let goalPlane = new GoalPlane(canvas1, coor1);
+let goal = new Goal(canvas1, coor1);
 let trajectory = new Trajectory(canvas1, coor1);
 
 ParaInpHandler.buildHandlers();
-/*
+
 // for fun
-setInterval(() => {
-    StatePara.theta += 10;
-}, 1000 / 20);
-*/
+//forFun();
+function forFun() {
+    setInterval(() => {
+        StatePara.theta += 2;
+    }, 1000 / 20);
+}
+
