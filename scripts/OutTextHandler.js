d605addae4d30fb4b5f8c1ab16b7ea0f48561ecf
cottonnfaiii <phafai.nunmanus@gmail.com>
2024-05-24 16:33:28 +0700

    update_shadow

--- scripts/OutTextHandler.js
+++ scripts/OutTextHandler.js
@@ -38,7 +38,10 @@ class OutTextHandler {
         // instantiate each handler as a static property
         this.theta_handler = new OutTextHandler('outTxt_theta', StatePara.state_theta);
         this.r_handler = new OutTextHandler('outTxt_r', StatePara.state_r);
+        this.z_robot_handler = new OutTextHandler('outTxt_z_robot', StatePara.state_z_robot);
+        this.x_goal_handler = new OutTextHandler('outTxt_x_goal', StatePara.state_x_goal);
         this.y_goal_handler = new OutTextHandler('outTxt_y_goal', StatePara.state_y_goal);
+        this.z_goal_handler = new OutTextHandler('outTxt_z_goal', StatePara.state_z_goal);
 
         // results thae must be calculated
         this.y_intercept_handler = new OutTextHandler('outTxt_y_intercept', null, function(state) {
@@ -59,6 +62,18 @@ class OutTextHandler {
             let error_y_cm = (y_f - y_goal) * 100;
             this.setText(error_y_cm.toFixed(2));
         });
+
+        this.cmpsted_theta_handler = new OutTextHandler('outTxt_cmpsted_theta', null, function(state) {
+            this.setText((StatePara.theta + StatePara.cmpst_theta).toFixed(2));
+        });
+
+        this.cmpsted_r_handler = new OutTextHandler('outTxt_cmpsted_r', null, function(state) {
+            this.setText((StatePara.r + StatePara.cmpst_r).toFixed(4));
+        })
+
+        this.cmpsted_z_robot_handler = new OutTextHandler('outTxt_cmpsted_z_robot', null, function(state) {
+            this.setText((StatePara.z_robot + StatePara.cmpst_z_robot).toFixed(4));
+        })
     }
 
 }
